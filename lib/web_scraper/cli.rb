c221b7bfe62b7fdfd9eb1305f769e78287ae53c3
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-04-08 18:03:23 -0500

    added short commands

--- lib/web_scraper/cli.rb
+++ lib/web_scraper/cli.rb
@@ -4,8 +4,8 @@ attr_accessor :news
 
   USER_DISPLAY = {
     intro_header: "Here are today's news headlines:\n\n",
-    main_input_options: "\n\nType the number of the acticle you want to learn more about.\n\nOther Available Commands:\n\nrefresh\nset country\nexit\n\nWhat would you like to do?",
-    detail_input_options: "\n\nType 'list' to return or 'open' to visit the webpage for this article.",
+    main_input_options: "\n\nType the number of the acticle you want to learn more about.\n\nOther Available Commands:\n\nr: refresh\nsc: set country\ne: exit\n\nWhat would you like to do?",
+    detail_input_options: "\n\nType 'list' or 'l' to return or 'open' or 'o' to visit the webpage for this article.",
     country_input_options: "\n\nWhat country would you like to see news for?\n\n",
     not_found: "\n\nItem Not Found\n\n",
     exit: "\n\nGood Bye!\n\n"
@@ -29,30 +29,35 @@ attr_accessor :news
   def input_handler
     input = nil
     article_num = nil
-    while input != "exit"
+    while input != "exit" || input != "e"
       input = gets.strip.downcase
       if input.to_i > 0 && input.to_i <= news.total_results - 1
           article_num = input.to_i - 1
           article = @news.articles[article_num]
           puts "#{article['description']}"
           puts USER_DISPLAY[:detail_input_options]
-      elsif input == "list"
+      elsif input == "list" || input == "l"
         list
-      elsif input == "open"
+      elsif input == "open" || input == "o"
         puts article_num
         open_article(article_num)
-      elsif input == "refresh"
+      elsif input == "refresh" || input == "r"
         @news = News.new
         list
-      elsif input == "set country"
+      elsif input == "set country" || input == "sc"
         puts USER_DISPLAY[:country_input_options]
-      elsif input == "exit"
+      elsif input == "exit" || input == 'e'
         close_program
       else
         puts USER_DISPLAY[:not_found]
         list
       end
     end
+  end
+
+  def country_input_handler
+    # Get input
+
   end
 
   def open_article(article_num)
